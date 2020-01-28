e9fd8d58dd71e6c0be7515646c9ada481fc2d7a1
DustinG98 <thedustingraham@gmail.com>
2020-01-28 10:15:59 -0700

    Card Context, and remove item from shopping cart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Context
 import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 function App() {
 	const [products] = useState(data);
@@ -19,22 +20,28 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={{ cart, removeItem }}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
